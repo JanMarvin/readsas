7be5467fd77d95880633c09cb4a0ff7a771ad7f3
Jan Marvin Garbuszus <jan.garbuszus@ruhr-uni-bochum.de>
2020-06-08 03:35:48 +0200

    case 3 for every case

--- src/readsas.cpp
+++ src/readsas.cpp
@@ -1304,7 +1304,7 @@ Rcpp::List readsas(const char * filePath,
               fmts.LEN = readbin(fmts.LEN, sas, swapit);
 
               if (debug)
-                Rcout << fmts.IDX << ", " << fmts.OFF <<
+                Rcout << "fmts: "  << fmts.IDX << ", " << fmts.OFF <<
                   ", " << fmts.LEN << std::endl;
 
               fmt.push_back(fmts);
@@ -1314,7 +1314,7 @@ Rcpp::List readsas(const char * filePath,
               lbls.LEN = readbin(lbls.LEN, sas, swapit);
 
               if (debug)
-                Rcout << lbls.IDX << ", " << lbls.OFF <<
+                Rcout << "lbls: "  << lbls.IDX << ", " << lbls.OFF <<
                   ", " << lbls.LEN << std::endl;
 
               lbl.push_back(lbls);
