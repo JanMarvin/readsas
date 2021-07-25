7abdc377f4f50c44955462230c222071014afd5f
Jan Marvin Garbuszus <jan.garbuszus@ruhr-uni-bochum.de>
2021-07-25 15:35:39 +0200

    unit tests for select.rows

--- tests/testthat/test_read.R
+++ tests/testthat/test_read.R
@@ -1,9 +1,10 @@
 
+### basic read
 fl <- system.file("extdata", "cars.sas7bdat", package = "readsas")
 
 ds <- read.sas(fl)
 
-test_that("compare", {
+test_that("read sas", {
   expect_true(all.equal(cars, ds, check.attributes = FALSE))
 })
 
@@ -50,3 +51,24 @@ test_that("read file with deleted rows", {
   expect_equal(exp, got, check.attributes = FALSE)
 
 })
+
+
+### select rows
+fl <- system.file("extdata", "mtcars.sas7bdat", package="readsas")
+
+dd <- read.sas(fl, select.rows = c(2,5))[-1]
+
+
+test_that("select.rows", {
+  expect_true(all.equal(dd, mtcars[2:5,], check.attributes = FALSE))
+})
+
+
+# ### select rows # FIXME: crashing
+# fl <- system.file("extdata", "mtcars.sas7bdat", package="readsas")
+#
+# dd <- read.sas(fl, select.cols = "MPG", debug = F)
+#
+# test_that("select.rows", {
+#   expect_true(all.equal(dd, mtcars[2:5,], check.attributes = FALSE))
+# })
