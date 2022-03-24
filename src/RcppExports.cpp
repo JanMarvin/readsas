308c8fcc6362559f71cd3e809f96f93ef4a7e39d
Jan Marvin Garbuszus <jan.garbuszus@rub.de>
2022-03-24 08:31:52 +0100

    Update RcppExports.cpp

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -5,6 +5,11 @@
 
 using namespace Rcpp;
 
+#ifdef RCPP_USE_GLOBAL_ROSTREAM
+Rcpp::Rostream<true>&  Rcpp::Rcout = Rcpp::Rcpp_cout_get();
+Rcpp::Rostream<false>& Rcpp::Rcerr = Rcpp::Rcpp_cerr_get();
+#endif
+
 // readsas
 Rcpp::List readsas(const char * filePath, const bool debug, const int64_t kk);
 RcppExport SEXP _readsas_readsas(SEXP filePathSEXP, SEXP debugSEXP, SEXP kkSEXP) {
