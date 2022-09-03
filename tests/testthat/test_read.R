f6fb7496a81d1e7f03a5111bef7778d3e598cf6f
Jan Marvin Garbuszus <jan.garbuszus@ruhr-uni-bochum.de>
2022-09-03 13:08:50 +0200

    fix for testthat e3 ignore_attr

--- tests/testthat/test_read.R
+++ tests/testthat/test_read.R
@@ -15,37 +15,37 @@ test_that("read file with deleted rows", {
   fl <- system.file("extdata", "test.sas7bdat", package = "readsas")
   exp <- data.frame(x = 1:3)
   got <- read.sas(fl)
-  expect_equal(exp, got, check.attributes = FALSE)
+  expect_equal(exp, got, ignore_attr = TRUE)
 
   # read file with deleted: delete x = 2
   fl <- system.file("extdata", "test2.sas7bdat", package = "readsas")
   exp <- data.frame(x = 1:3)[c(1, 3), , drop = FALSE]
   got <- read.sas(fl)
-  expect_equal(exp, got, check.attributes = FALSE)
+  expect_equal(exp, got, ignore_attr = TRUE)
 
   # read file with deleted: delete x > 1
   fl <- system.file("extdata", "test3.sas7bdat", package = "readsas")
   exp <- data.frame(x = 1:3)[c(1), , drop = FALSE]
   got <- read.sas(fl)
-  expect_equal(exp, got, check.attributes = FALSE)
+  expect_equal(exp, got, ignore_attr = TRUE)
 
   # read file with deleted: delete x = 1
   fl <- system.file("extdata", "test4.sas7bdat", package = "readsas")
   exp <- data.frame(x = 1:3)[-c(1), , drop = FALSE]
   got <- read.sas(fl)
-  expect_equal(exp, got, check.attributes = FALSE)
+  expect_equal(exp, got, ignore_attr = TRUE)
 
   # read file with deleted: delete x < 3
   fl <- system.file("extdata", "test5.sas7bdat", package = "readsas")
   exp <- data.frame(x = 1:3)[-c(1, 2), , drop = FALSE]
   got <- read.sas(fl)
-  expect_equal(exp, got, check.attributes = FALSE)
+  expect_equal(exp, got, ignore_attr = TRUE)
 
   # skip row
   fl <- system.file("extdata", "test2.sas7bdat", package = "readsas")
   exp <- data.frame(x = 1:3)[c(1), , drop = FALSE]
   got <- read.sas(fl, select.rows = c(1, 2))
-  expect_equal(exp, got, check.attributes = FALSE)
+  expect_equal(exp, got, ignore_attr = TRUE)
 
 })
 
